2ad24e835ee2ee65a9210f4cc10025789b844db2
chenDoInG <chendoing@gmail.com>
2015-12-21 14:27:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# springboot-gradle-learning
+
+learning springboot and gradle
+and deploy to Tomcat 8
---
+++ src/main/java/com/chendoing/configuration/JspConfig.java
@@ -0,0 +1,20 @@
+package com.chendoing.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class JspConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
